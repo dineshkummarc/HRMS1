f9cf946008799ecf0180d88f127871a6f8787b2b
Dinesh Kumar <mapmydestination@gmail.com>
2025-08-25 08:43:39 +0530

    update

--- admin/includes/conn.php
+++ admin/includes/conn.php
@@ -1,5 +1,5 @@
 <?php
-	$conn = new mysqli('127.0.0.1:3325', 'root', '', 'apsystem');
+	$conn = new mysqli('127.0.0.1:3306', 'root', '', 'apsystem');
 
 	if ($conn->connect_error) {
 	    die("Connection failed: " . $conn->connect_error);
